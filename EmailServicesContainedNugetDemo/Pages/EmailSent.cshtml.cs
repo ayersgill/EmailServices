2bd8faf03a91b7cc44176adc54556d12b15c8eac
Larry McCartin <larry.mccartin@oregon.gov>
2020-10-21 08:40:21 -0700

    Added demo project to use new 3.1 specific library

---
+++ EmailServicesContainedNugetDemo/Pages/EmailSent.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using DASIT.EmailServices;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmailServicesContainedNugetDemo.Pages
+{
+    public class EmailSentModel : PageModel
+    {
+
+      
+
+        public EmailSentModel()
+        {
+           
+        }
+
+       
+
+        public async Task<IActionResult> OnGet()
+        {
+            return Page();
+        }
+
+       
+    }
+}
