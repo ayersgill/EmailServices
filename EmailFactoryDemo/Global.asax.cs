755eb802b8de0230718de03cb8f3ef540edbef74
Larry McCartin <larry.mccartin@oregon.gov>
2021-04-07 13:37:56 -0700

    Attempting to add logging to Framework Demo to figure out error on dev.

--- EmailFactoryDemo/Global.asax.cs
+++ EmailFactoryDemo/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,13 @@ namespace EmailFactoryDemo
     {
         protected void Application_Start()
         {
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+
+            Log.Information("here");
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
